ad79ef04b388cbf8573b72c95f825d206bde3601
Akshansh Jain <akshanshjn9@gmail.com>
2017-12-21 22:34:15 +0530

    Initialising GitHub

---
+++ app/src/main/java/com/madhouseapps/engineeringconvert/ConversionsActivity.java
@@ -0,0 +1,13 @@
+package com.madhouseapps.engineeringconvert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConversionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conversions);
+    }
+}
