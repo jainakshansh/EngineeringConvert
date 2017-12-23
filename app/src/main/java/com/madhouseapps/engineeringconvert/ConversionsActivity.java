ac955342e0752b01ba466ad23784aa1e170a29d1
Akshansh Jain <akshanshjn9@gmail.com>
2017-12-23 17:07:00 +0530

    Layout completed. Init complete

--- app/src/main/java/com/madhouseapps/engineeringconvert/ConversionsActivity.java
+++ app/src/main/java/com/madhouseapps/engineeringconvert/ConversionsActivity.java
@@ -1,13 +1,34 @@
 package com.madhouseapps.engineeringconvert;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.AppCompatSpinner;
+import android.widget.Button;
 
 public class ConversionsActivity extends AppCompatActivity {
+
+    private AppCompatSpinner fromSpinner, toSpinner;
+    private AppCompatEditText fromEdit, toEdit;
+    private Button swapButton;
+    private int from = 0, to = 0;
+    private Typeface typeface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversions);
+
+        fromSpinner = findViewById(R.id.fromSpinner);
+        toSpinner = findViewById(R.id.toSpinner);
+        fromEdit = findViewById(R.id.fromEditText);
+        toEdit = findViewById(R.id.toEditText);
+        swapButton = findViewById(R.id.swap_button);
+        typeface = Typeface.createFromAsset(getAssets(), "fonts/quicksand_medium.ttf");
+        fromEdit.setTypeface(typeface);
+        toEdit.setTypeface(typeface);
+
+
     }
 }
