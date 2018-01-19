7390d5a190e65b038c36b7426461dfcb970e4406
Akshansh Jain <akshanshjn9@gmail.com>
2018-01-19 22:56:44 +0530

    Added list items and copy code

--- app/src/main/java/com/madhouseapps/engineeringconvert/Adapters/ConstantRecyclerAdapter.java
+++ app/src/main/java/com/madhouseapps/engineeringconvert/Adapters/ConstantRecyclerAdapter.java
@@ -1,12 +1,16 @@
 package com.madhouseapps.engineeringconvert.Adapters;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.graphics.Typeface;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.madhouseapps.engineeringconvert.ConstantItem;
 import com.madhouseapps.engineeringconvert.R;
@@ -24,12 +28,14 @@ public class ConstantRecyclerAdapter extends RecyclerView.Adapter<ConstantRecycl
     private Typeface quicksand_medium;
 
     public class ConstantViewHolder extends RecyclerView.ViewHolder {
-        public TextView constant, value;
+        public TextView name, value;
+        public ImageView symbol;
 
         public ConstantViewHolder(View view) {
             super(view);
-            constant = view.findViewById(R.id.constant_name);
+            name = view.findViewById(R.id.constant_name);
             value = view.findViewById(R.id.constant_value);
+            symbol = view.findViewById(R.id.constant_symbol);
         }
     }
 
@@ -47,12 +53,26 @@ public class ConstantRecyclerAdapter extends RecyclerView.Adapter<ConstantRecycl
 
     @Override
     public void onBindViewHolder(ConstantViewHolder holder, int position) {
-        ConstantItem item = constantItemList.get(position);
+        final ConstantItem item = constantItemList.get(position);
 
-        holder.constant.setTypeface(quicksand_medium);
-        holder.constant.setText(item.getName());
+        holder.name.setTypeface(quicksand_medium);
+        holder.name.setText(item.getName());
         holder.value.setTypeface(quicksand_medium);
         holder.value.setText(item.getValue());
+        holder.symbol.setImageResource(item.getSymbol());
+
+        /*
+        Code to copy the value of the constant when clicked.
+         */
+        holder.value.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ClipboardManager clipboardManager = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clipData = ClipData.newPlainText("Value copied", item.getValue());
+                clipboardManager.setPrimaryClip(clipData);
+                Toast.makeText(context, item.getName() + " value copied to clipboard!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/madhouseapps/engineeringconvert/ConstantsActivity.java
+++ app/src/main/java/com/madhouseapps/engineeringconvert/ConstantsActivity.java
@@ -35,11 +35,28 @@ public class ConstantsActivity extends AppCompatActivity {
     }
 
     private void getData() {
-        constantItemList.add(new ConstantItem("Proton Mass (mp)", "1.672621777 E-27"));
-        constantItemList.add(new ConstantItem("Neutron Mass (mn)", "1.674927351 E-27"));
-        constantItemList.add(new ConstantItem("Electron Mass (me)", "9.10938291 E-31"));
-        constantItemList.add(new ConstantItem("Muon Mass (mu)", "1.883531475 E-28"));
-        constantItemList.add(new ConstantItem("Planck Constant (h)", "6.62606957 E-34"));
+        constantItemList.add(new ConstantItem("Mass of Proton", "1.672621777 E-27 \nkg", R.drawable.proton_mass));
+        constantItemList.add(new ConstantItem("Mass of Neutron", "1.674927351 E-27 \nkg", R.drawable.neutron_mass));
+        constantItemList.add(new ConstantItem("Mass of Electron", "9.10938291 E-31 \nkg", R.drawable.electron_mass));
+        constantItemList.add(new ConstantItem("Mass of Muon", "1.883531475 E-28 \nkg", R.drawable.muon_mass));
+        constantItemList.add(new ConstantItem("Planck's Constant", "6.62606957 E-34 \nJ/Hz", R.drawable.plancks_constant));
+        constantItemList.add(new ConstantItem("Bohr Radius", "5.291772109 E-11 \nm", R.drawable.bohr_radius));
+        constantItemList.add(new ConstantItem("Nuclear Magneton", "5.05078353 E-27 \nJ/T", R.drawable.nuclear_magneton));
+        constantItemList.add(new ConstantItem("Bohr Magneton", "9.27400968 E-24 \nJ/T", R.drawable.bohr_magenton));
+        constantItemList.add(new ConstantItem("Compton Wavelength", "2.426310239 E-12 \nm", R.drawable.compton_wavelength));
+        constantItemList.add(new ConstantItem("Rydberg's Constant", "10973731.57 \n1/m", R.drawable.rydbergs_constant));
+        constantItemList.add(new ConstantItem("Atomic Mass Unit", "1.660538921 E-27 \nkg", R.drawable.atomic_mass_unit));
+        constantItemList.add(new ConstantItem("Faraday's Constant", "96485.3365 \nC/mol", R.drawable.faradays_constant));
+        constantItemList.add(new ConstantItem("Avogadro Number", "6.02214129 E23 \n1/mol", R.drawable.avogadro_number));
+        constantItemList.add(new ConstantItem("Boltzmann's Constant", "1.3806488 E-23 \nJ/K", R.drawable.boltzmann_constant));
+        constantItemList.add(new ConstantItem("Ideal Gas Constant", "8.3144621 \nJ/K/mol", R.drawable.ideal_gas_constant));
+        constantItemList.add(new ConstantItem("Stefan's Constant", "5.670373 E-8", R.drawable.stefans_constant));
+        constantItemList.add(new ConstantItem("Electric Constant", "8.854187817 E-12 \nF/m", R.drawable.electric_constant));
+        constantItemList.add(new ConstantItem("Magnetic Constant", "1.256637061 E-6 \nH/m", R.drawable.magnetic_constant));
+        constantItemList.add(new ConstantItem("Quantum of Flux", "2.067833758 E-15 \nWb", R.drawable.quantum_flux));
+        constantItemList.add(new ConstantItem("Normal Gravity", "9.80665 \nN/kg", R.drawable.gravity));
+        constantItemList.add(new ConstantItem("Newton's Constant", "6.67384 E-11", R.drawable.newtons_constant));
+        constantItemList.add(new ConstantItem("Normal Pressure", "101325 \nPa", R.drawable.atmosphere));
     }
 
     @Override
