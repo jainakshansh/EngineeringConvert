2f6004c26caa154307cf7bf9a32393915493a938
Akshansh Jain <akshanshjn9@gmail.com>
2018-01-20 20:24:23 +0530

    Added landing screen

--- app/src/main/java/com/madhouseapps/engineeringconvert/LandingActivity.java
+++ app/src/main/java/com/madhouseapps/engineeringconvert/LandingActivity.java
@@ -1,23 +1,50 @@
 package com.madhouseapps.engineeringconvert;
 
 import android.content.Intent;
+import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LandingActivity extends AppCompatActivity {
+
+    private TextView madText, houseText;
+    private Button share, rate;
+
+    private Typeface quicksand;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
-    }
 
-    private void constantsClass(View view) {
-        startActivity(new Intent(getApplicationContext(), ConstantsActivity.class));
-    }
+        quicksand = Typeface.createFromAsset(getAssets(), "fonts/quicksand_medium.ttf");
+        madText = findViewById(R.id.mad_text);
+        houseText = findViewById(R.id.house_text);
+        madText.setTypeface(quicksand);
+        houseText.setTypeface(quicksand, Typeface.BOLD);
 
-    private void conversionsClass(View view) {
-        startActivity(new Intent(getApplicationContext(), ConversionsActivity.class));
+        share = findViewById(R.id.share_button);
+        rate = findViewById(R.id.rate_button);
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent shareIntent = new Intent();
+                shareIntent.setAction(Intent.ACTION_SEND);
+                shareIntent.putExtra(Intent.EXTRA_TEXT,
+                        "Hey, Check out this amazing Engineering Unit Converter at: https://play.google.com/store/apps/details?id=com.madhouseapps.engineeringconvert");
+                shareIntent.setType("text/plain");
+                startActivity(shareIntent);
+            }
+        });
+        rate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.madhouseapps.engineeringconvert")));
+            }
+        });
     }
 }
